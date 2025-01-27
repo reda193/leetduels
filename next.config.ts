b2211010ae0405a9f0806344261b6e08646af099
reda193 <mohamed.reda33@outlook.com>
2025-01-27 16:52:38 -0500

    work

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-
+  pageExtensions: [
+    "page.tsx", // Your custom extension
+    "page.ts",
+    "ts" // Required for `not-found.ts`
+],
 };
 
 export default nextConfig;
