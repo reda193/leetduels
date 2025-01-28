55b744afd9dc7b67bfa690f9627c358caeb1eab4
reda193 <mohamed.reda33@outlook.com>
2025-01-27 23:03:38 -0500

    weqeqw

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,7 @@
-'use client';
-
-import Link from 'next/link';
+export const dynamic = "force-dynamic";
+import { redirect } from "next/navigation";
 
 export default function NotFound() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h2 className="text-2xl font-bold mb-4">404 - Page Not Found</h2>
-      <p className="mb-4">The page you are looking for does not exist.</p>
-      <Link href="/" className="text-blue-500 hover:text-blue-700">
-        Return to Home
-      </Link>
-    </div>
-  );
+  redirect("/home");
+  return <div></div>;
 }
